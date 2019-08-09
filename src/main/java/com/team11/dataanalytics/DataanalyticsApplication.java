db08aa45756813b6ff6216e84d26b0c1bbab77c5
laoguo <1187565074@qq.com>
2019-08-09 10:47:27 +0800

    first commit

---
+++ src/main/java/com/team11/dataanalytics/DataanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.team11.dataanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataanalyticsApplication.class, args);
+	}
+
+}
