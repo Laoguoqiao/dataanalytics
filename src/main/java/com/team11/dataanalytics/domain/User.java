de0f2d4cb3747308b05f1dd4cf09f2393c2964e8
KangShiJi <kksj3100@163.com>
2019-08-12 08:48:59 +0800

    1st try

--- src/main/java/com/team11/dataanalytics/domain/User.java
+++ src/main/java/com/team11/dataanalytics/domain/User.java
@@ -1,4 +1,43 @@
 package com.team11.dataanalytics.domain;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@GenericGenerator(name = "jpa-uuid", strategy = "uuid")
 public class User {
+    @Id
+    @NotNull
+    @GeneratedValue(generator = "jpa-uuid")
+    private String uid;
+    private String account;
+    private String password;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
